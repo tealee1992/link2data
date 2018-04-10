ab169d0850a941cacbbe2adad6b0aec10f110ce6
tealee1992 <tealee1992@163.com>
2018-04-10 17:48:28 +0800

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  BACKEND_API_ROOT: 'http://localhost:9900/',
-  HDFS_BACKEND: 'http://11.0.17.199:8080/',
+  BACKEND_API_ROOT: 'http://100.64.184.211:19900/',
+  HDFS_BACKEND: 'http://100.64.184.211:18080/',
 }
 
 module.exports = Config
