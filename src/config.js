48c9eb0ad3171957f481947f3899597131eba419
tealee1992 <tealee1992@163.com>
2018-04-29 16:49:56 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
   BACKEND_API_ROOT: 'http://datastore.api.heyeah.cn:8083/',
-  HDFS_BACKEND: 'http://hdfs.api.heyeah.cn:8083/HadoopInfo',
+  HDFS_BACKEND: 'http://hdfs.api.heyeah.cn:8083/HadoopInfo/',
 }
 
 module.exports = Config
